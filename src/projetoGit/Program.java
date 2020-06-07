5a5809cb238cdf5fbfd5d8cae8ec580e5492dccd
ben10debian <ben-hur.figueiredo@hotmail.com>
2020-06-07 14:22:06 -0300

    Modificado a mensagem de Saída

--- src/projetoGit/Program.java
+++ src/projetoGit/Program.java
@@ -7,9 +7,9 @@ public class Program {
 	public static void main(String[] args) {
 		// TODO Stub de método gerado automaticamente
 
-		System.out.println("Olá");
+		System.out.println("Olá! Agora está guitado!");
 		
-		JOptionPane.showMessageDialog(null, "Olá");
+		JOptionPane.showMessageDialog(null, "Olá agora está guitado!");
 		
 		
 		
