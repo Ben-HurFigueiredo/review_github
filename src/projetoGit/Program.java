f21e4f411c1dbdeadd1284571f922150f745a9e2
ben10debian <ben-hur.figueiredo@hotmail.com>
2020-06-07 12:02:31 -0300

    Projeto Criado

---
+++ src/projetoGit/Program.java
@@ -0,0 +1,18 @@
+package projetoGit;
+
+import javax.swing.JOptionPane;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Stub de método gerado automaticamente
+
+		System.out.println("Olá");
+		
+		JOptionPane.showMessageDialog(null, "Olá");
+		
+		
+		
+	}
+
+}
---
+++ src/projetoGit/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author ben10debian
+ *
+ */
+package projetoGit;
